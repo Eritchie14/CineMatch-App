36f905a89827a0660e36dab08748e3192158dc3f
Eritchie14 <evanritchie14@gmail.com>
2026-05-08 13:41:49 -0600

    Linked backend to frontend

--- Backend/main.py
+++ Backend/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from services.api_service import ApiService
 from services.tmdb_service import TMDBService
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # loads env variables in .env
-load_dotenv()
+#load_dotenv()
 
 # initialize services
 api_service = ApiService()
@@ -32,3 +32,20 @@ async def test():
 @app.get("/movies/sam")
 async def get_sam_movies():
     return tmdb_service.get_sam_movies()
+
+@app.get("/movies/top-rated")
+async def get_top_rated_movies():
+    return tmdb_service.get_top_rated_movies()
+
+@app.get("/movies/now-playing")
+async def get_now_playing_movies():
+    return tmdb_service.get_now_playing_movies()
+
+@app.get("/movies/popular")
+async def get_popular_movies():
+    return tmdb_service.get_popular_movies()
+
+@app.get("/movies/{movie_id}")
+async def get_movie_details(movie_id: int):
+    return tmdb_service.get_movie_details(movie_id)
+
--- Backend/services/tmdb_service.py
+++ Backend/services/tmdb_service.py
@@ -2,6 +2,11 @@ import os
 import requests
 import json
 
+from dotenv import load_dotenv
+
+# loads env variables in .env
+load_dotenv()
+
 TMDB_API_READ_TOKEN = os.getenv("TMDB_API_READ_TOKEN")
 
 class TMDBService:
@@ -16,3 +21,30 @@ class TMDBService:
 
         response = requests.get(url, headers=self.headers)
         return json.loads(response.text)
+    
+    def get_movie_details(self, movie_id):
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+
+        response = requests.get(url, headers=self.headers)
+        return json.loads(response.text)
+    
+    def get_top_rated_movies(self):
+        url = "https://api.themoviedb.org/3/movie/top_rated"
+
+        response = requests.get(url, headers=self.headers)
+        response = json.loads(response.text)
+        return response.get("results", [])[:10]
+    
+    def get_now_playing_movies(self):
+        url = "https://api.themoviedb.org/3/movie/now_playing"
+
+        response = requests.get(url, headers=self.headers)
+        response = json.loads(response.text)
+        return response.get("results", [])[:10]
+    
+    def get_popular_movies(self):
+        url = "https://api.themoviedb.org/3/movie/popular"
+
+        response = requests.get(url, headers=self.headers)
+        response = json.loads(response.text)
+        return response.get("results", [])[:10]
